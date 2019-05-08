78e7e0dda2d3b57d6e2b2405caaee987c8229466
Ednei de Freitas Neto <ednei.freitas@moebius.com.br>
2019-05-08 00:10:52 -0300

    Initial commit

---
+++ app/src/main/java/com/example/aluraconstrantlayout/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,14 @@
+package com.example.aluraconstrantlayout.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.aluraconstrantlayout.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
